a7face84713c2f4fd36b4af489144844580dd0ee
Rick Mann <rmann@latencyzero.com>
2014-07-07 14:10:10 -0700

    Eh.

--- follows.js
+++ follows.js
@@ -12,13 +12,13 @@
 		hideForm		= 0;
 	*/
 
-	var defaultKey		= '', // Unique master Xively API key to be used as a default
-		defaultFeeds	= [], // Comma separated array of Xively Feed ID numbers
-		applicationName	= '', // Replaces Xively logo in the header
-		dataDuration	= '', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
+	var defaultKey		= '7mJyjkX1nbcBSbJAS1XfjxwMT0cttfqLRRsvuQJ1ooxNDD50', // Unique master Xively API key to be used as a default
+		defaultFeeds	= [2021806591], // Comma separated array of Xively Feed ID numbers
+		applicationName	= 'The Future’s Solar Power Station', // Replaces Xively logo in the header
+		dataDuration	= '2days', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
 		dataInterval	= 0, // Default interval for data to be displayed (in seconds)
 		dataColor		= '', // CSS HEX value of color to represent data (omit leading #)
-		hideForm		= 0; // To hide input form use value of 1, otherwise set to 0
+		hideForm		= 1; // To hide input form use value of 1, otherwise set to 0
 
 // Function Declarations
 
